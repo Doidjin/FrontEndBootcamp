be81c3d9a075b79a7ab01d2b696e1fc51b13003a
Leonardo Cabbau <leocabbau2@gmail.com>
2017-11-17 22:41:21 +0000

    making the register and login form and using AUTH

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 var Campground = require("./models/campground");
 var Comment = require('./models/comment');
 var seedDB = require('./seeds');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
 
 
 //Creating the Connection
@@ -14,6 +17,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 seedDB();
+
+// ===== PASSPORT CONFIGURATION =====
+app.use(require('express-session')({
+    secret: "Gostaria que todo mundo morresse",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //Root path
 app.get('/', function(req, res){
@@ -101,6 +117,39 @@ app.post('/campgrounds/:id/comments', function(req, res){
             });
         }
     });
+    
+});
+
+// ============== AUTH ROUTES ================
+// show register form
+app.get('/register', function(req, res){
+   res.render('register'); 
+});
+
+//handle sign up logic
+app.post('/register', function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render('register');
+       } else{
+           passport.authenticate('local')(req, res, function(){
+               res.redirect('/campgrounds');
+           });
+       }
+    });
+});
+
+//show login form
+app.get('/login', function(req, res){
+   res.render('login'); 
+});
+
+//handle login logic - middleware
+app.post('/login', passport.authenticate('local',{
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+}), function(req, res){
     
 });
 
